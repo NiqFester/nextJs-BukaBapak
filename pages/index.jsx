9909e93e82d152e7204cb9833cc14de3f748a110
NiqFester <nickfester010@gmail.com>
2023-03-31 21:46:12 -0700

    modified:   pages/index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,31 +14,36 @@ export default function Home({ data }) {
 
   return (
     <>
-      <Kategori products={data} />
-      <Banner />
-      <IconMenu />
-      <div className="bg-gradient-to-r from-red-700 to-purple-900 w-[90vw] h-36 mx-auto relative my-8">
-        <button className="border text-white font-bold rounded text-center absolute right-[80px] bottom-[50px] px-5 py-2 text-lg">
-          Ambil Tahun Depan {">"}
-        </button>
+      <div className="sm:hidden">
+        <Kategori products={data} />
+        <Banner />
+        <IconMenu />
+        <div className="bg-gradient-to-r from-red-700 to-purple-900 w-[90vw] h-36 mx-auto relative my-8">
+          <button className="border text-white font-bold rounded text-center absolute right-[80px] bottom-[50px] px-5 py-2 text-lg">
+            Ambil Tahun Depan {">"}
+          </button>
+        </div>
+        <FlashDeal data={data} />
+        <Mall />
+        <Popular />
+        <Unggulan data={data} />
+        <BestDeal data={data} />
+        <div className="flex justify-between mx-8 my-4 border-t-4 pt-12 ">
+          <p className="font-bold text-2xl">Pencarian Populer</p>
+          <p className="text-sm font-bold text-blue-300">Lihat Semua</p>
+        </div>
+        <div className="flex gap-4 m-8">
+          <button className="border border-slate-300 px-2 py-1">Miaw</button>
+          <button className="border border-slate-300 px-2 py-1">asdaeek</button>
+          <button className="border border-slate-300 px-2 py-1">Pacar</button>
+          <button className="border border-slate-300 px-2 py-1">
+            Pendamping Hidup
+          </button>
+          <button className="border border-slate-300 px-2 py-1">Teman</button>
+        </div>
       </div>
-      <FlashDeal data={data} />
-      <Mall />
-      <Popular />
-      <Unggulan data={data} />
-      <BestDeal data={data} />
-      <div className="flex justify-between mx-8 my-4 border-t-4 pt-12 ">
-        <p className="font-bold text-2xl">Pencarian Populer</p>
-        <p className="text-sm font-bold text-blue-300">Lihat Semua</p>
-      </div>
-      <div className="flex gap-4 m-8">
-        <button className="border border-slate-300 px-2 py-1">Miaw</button>
-        <button className="border border-slate-300 px-2 py-1">asdaeek</button>
-        <button className="border border-slate-300 px-2 py-1">Pacar</button>
-        <button className="border border-slate-300 px-2 py-1">
-          Pendamping Hidup
-        </button>
-        <button className="border border-slate-300 px-2 py-1">Teman</button>
+      <div className="sm:block hidden">
+          <p className="text-center mt-[50vh] text-4xl"> Maaf tapi Web ini hanya bisa dilihat di Pc</p>
       </div>
     </>
   );
